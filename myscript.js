acc30a4cd20ee470ddfcc75fe9cf790c4743ed17
Masadavide <davide.masadm@gmail.com>
2022-01-17 17:44:05 +0100

    manca ultimo bonus

--- myscript.js
+++ myscript.js
@@ -1,9 +1,35 @@
-let app = new VTTCue({
+let app = new Vue({
     el: '#app',
-    data:[
-        toDoArray: '',
-    ],
+    data:{
+        array:"",
+        toDo:[
+            {
+                text: 'sbarrato',
+                done: true,
+            },
+            {
+                text: 'non sbarrato',
+                done: false,
+            },
+            {
+                text: '3',
+                done: true,
+            }
+        ]
+    },
     methods:{
-        
-    }
+        fatto: function(indice){
+            
+            if(this.toDo[indice].done == true){
+                return 'sbarrato'
+                }
+        },
+        aggiungi: function(){
+            this.toDo.push({
+                text:this.array,
+                done: false,
+            })
+            this.array=""
+        }
+    },
 })
